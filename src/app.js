4698350e486c7e44bfc93db2eef0d5ff351b2c45
nahee-park <jessica396@ewhain.net>
2022-01-06 16:14:50 +0900

    🧭 [DEV] :  node 서버 띄우기, 기본 모듈 설치 #2

---
+++ src/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const app = express();
+
+dotenv.config();
+
+app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, content-type, x-access-token');
+  next();
+});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/', require('./routes'));
+
+app.use('*', (req, res) => {
+  res.status(404).json({
+    status: 404,
+    success: false,
+    message: '잘못된 경로입니다.',
+  });
+});
+
+app
+  .listen(8080, () => {
+    console.log('server is running');
+  })
+  .on('error', () => {
+    process.exit(1);
+  });
