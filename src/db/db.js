9b9b6a53d09ebe73246c84edb2f6013f039dfef2
nahee-park <jessica396@ewhain.net>
2022-01-09 14:21:34 +0900

    🧭 [DEV] : 초기 폴더 구조 세팅 #2

---
+++ src/db/db.js
@@ -0,0 +1,66 @@
+const { Pool, Query } = require('pg');
+const dayjs = require('dayjs');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const dbConfig = require('../config/dbConfig');
+
+let devMode = process.env.NODE_ENV === 'development';
+const sqlDebug = true;
+
+const submit = Query.prototype.submit;
+Query.prototype.submit = function () {
+  const text = this.text;
+  const values = this.values || [];
+  const query = text.replace(/\$([0-9]+)/g, (m, v) => JSON.stringify(values[parseInt(v) - 1]));
+  devMode && sqlDebug && console.log(`\n\n[👻 SQL STATEMENT]\n${query}\n_________\n`);
+  submit.apply(this, arguments);
+};
+
+console.log(`[🔥DB] ${process.env.NODE_ENV}`);
+
+const pool = new Pool({
+  ...dbConfig,
+  connectionTimeoutMillis: 60 * 1000,
+  idleTimeoutMillis: 60 * 1000,
+});
+
+const connect = async (req) => {
+  const now = dayjs();
+  const string =
+    !!req && !!req.method
+      ? `[${req.method}] ${!!req.user ? `${req.user.id}` : ``} ${req.originalUrl}\n ${!!req.query && `query: ${JSON.stringify(req.query)}`} ${!!req.body && `body: ${JSON.stringify(req.body)}`} ${
+          !!req.params && `params ${JSON.stringify(req.params)}`
+        }`
+      : `request 없음`;
+  const callStack = new Error().stack;
+  const client = await pool.connect();
+  const query = client.query;
+  const release = client.release;
+
+  const releaseChecker = setTimeout(() => {
+    devMode && console.error('[ERROR] client connection이 15초 동안 릴리즈되지 않았습니다.', { callStack });
+    devMode && console.error(`마지막으로 실행된 쿼리문입니다. ${client.lastQuery}`);
+  }, 15 * 1000);
+
+  client.query = (...args) => {
+    client.lastQuery = args;
+    return query.apply(client, args);
+  };
+  client.release = () => {
+    clearTimeout(releaseChecker);
+    const time = dayjs().diff(now, 'millisecond');
+    if (time > 4000) {
+      const message = `[RELEASE] in ${time} | ${string}`;
+      devMode && console.log(message);
+    }
+    client.query = query;
+    client.release = release;
+    return release.apply(client);
+  };
+  return client;
+};
+
+module.exports = {
+  connect,
+};
