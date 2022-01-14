11e4ddeedfc8c1ca67251883e4afa6e5c824081f
nahee-park <jessica396@ewhain.net>
2022-01-14 10:07:48 +0900

    ✨ [FEAT] :  테마별 소품샵 GET db 로직 개발 #7
    # ✨ [FEAT] : 새로운 기능 구현 #이슈번호
    # 🔨 [FIX] : 버그, 오류 해결 #이슈번호
    # 📝 [DOCS] : README나 WIKI 등의 문서 개정 #이슈번
    # 💄 [STYLE] :  코드 포맷팅, 세미콜론 누락, 코드 변경이 없는 경우 #이슈번호호
    # ♻️ [REFACTOR] : 코드 리펙토링 #이슈번호
    # ⚡️ [TEST] : 테스트 코드, 리펙토링 테스트 코드 추가 #이슈번호
    # ✅ [CHORE] : 빌드 업무 수정, 패키지 매니저 수정 #이슈번호
    # 🧭 [DEV] :  개발 환경 세팅 #이슈번호
    # ➕ [ADD] : Feat 이외의 부수적인 코드 추가, 라이브러리 추가, 새로운 파일 생성 시 #이슈번호
    # 🚑️ [HOTFIX] : issue나, QA에서 급한 버그 수정에 사용 #이슈번호
    # ⚰️ [DEL] : 쓸모없는 코드 삭제 #이슈번호
    # ✏️ [CORRECT] : 주로 문법의 오류나 타입의 변경, 이름 변경 등에 사용합니다. #이슈번호
    # 🚚 [MOVE] : 프로젝트 내 파일이나 코드의 이동 #이슈번호
    # ⏪️ [RENAME] : 파일 이름 변경이 있을 때 사용합니다. #이슈번호
    # 🔀 [MERGE]: 다른브렌치를 merge 할 때 사용합니다. #이슈번호
    # —————————
    #     제목은 명령문 대문자 영어로 적자.
    #     제목 끝에 마침표(.) 금지
    #     제목과 본문을 한 줄 띄워 분리하기
    #     본문은 한글로 적는다.
    #     본문은 "어떻게" 보다 "무엇을", "왜"를 설명한다.
    #     본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    # —————————

--- src/db/shop.js
+++ src/db/shop.js
@@ -21,4 +21,28 @@ const getShopByArea = async (client, area) => {
   return convertSnakeToCamel.keysToCamel(rows);
 };
 
-module.exports = { getShopByArea };
+const getShopByTheme = async (client, theme) => {
+  const { rows } = await client.query(
+    `
+          SELECT DISTINCT s.id, s.shop_name, c.name, si.image
+          FROM shop s 
+          INNER JOIN shop_category sc
+          ON s.id = sc.shop_id
+          INNER JOIN category c
+          ON sc.category_id = c.id
+          INNER JOIN shop_theme st
+          ON s.id = st.shop_id
+          INNER JOIN theme t
+          ON st.theme_id = t.id
+          INNER JOIN shop_image si
+          ON s.id = si.shop_id
+          WHERE t.name = $1
+              AND si.is_preview = TRUE
+              AND s.is_deleted = FALSE
+          `,
+    [theme],
+  );
+  return convertSnakeToCamel.keysToCamel(rows);
+};
+
+module.exports = { getShopByArea, getShopByTheme };
