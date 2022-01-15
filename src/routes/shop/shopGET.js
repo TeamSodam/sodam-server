56601dc862111f1e3e3d05a14bfed879383758cb
nahee-park <jessica396@ewhain.net>
2022-01-15 21:16:13 +0900

    ✨ [FEAT] : 위치별, 테마별 이미지 가져오는 GET로직 개발 #6, #7
    # ✨ [FEAT] : 새로운 기능 구현 #이슈번호
    # 🔨 [FIX] : 버그, 오류 해결 #이슈번호
    # 📝 [DOCS] : README나 WIKI 등의 문서 개정 #이슈번
    # 💄 [STYLE] :  코드 포맷팅, 세미콜론 누락, 코드 변경이 없는 경우 #이슈번호호
    # ♻️ [REFACTOR] : 코드 리펙토링 #이슈번호
    # ⚡️ [TEST] : 테스트 코드, 리펙토링 테스트 코드 추가 #이슈번호
    # ✅ [CHORE] : 빌드 업무 수정, 패키지 매니저 수정 #이슈번호
    # 🧭 [DEV] :  개발 환경 세팅 #이슈번호
    # ➕ [ADD] : Feat 이외의 부수적인 코드 추가, 라이브러리 추가, 새로운 파일 생성 시 #이슈번호
    # 🚑️ [HOTFIX] : issue나, QA에서 급한 버그 수정에 사용 #이슈번호
    # ⚰️ [DEL] : 쓸모없는 코드 삭제 #이슈번호
    # ✏️ [CORRECT] : 주로 문법의 오류나 타입의 변경, 이름 변경 등에 사용합니다. #이슈번호
    # 🚚 [MOVE] : 프로젝트 내 파일이나 코드의 이동 #이슈번호
    # ⏪️ [RENAME] : 파일 이름 변경이 있을 때 사용합니다. #이슈번호
    # 🔀 [MERGE]: 다른브렌치를 merge 할 때 사용합니다. #이슈번호
    # —————————
    #     제목은 명령문 대문자 영어로 적자.
    #     제목 끝에 마침표(.) 금지
    #     제목과 본문을 한 줄 띄워 분리하기
    #     본문은 한글로 적는다.
    #     본문은 "어떻게" 보다 "무엇을", "왜"를 설명한다.
    #     본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    # —————————

--- src/routes/shop/shopGET.js
+++ src/routes/shop/shopGET.js
@@ -3,6 +3,7 @@ const statusCode = require('../../constants/statusCode');
 const { shopDB } = require('../../db');
 const db = require('../../db/db');
 const util = require('../../lib/util');
+const { duplicatedDataClean } = require('../../lib/convertRawDataToProccessedData');
 
 module.exports = async (req, res) => {
   const { area, theme } = req.query;
@@ -17,11 +18,56 @@ module.exports = async (req, res) => {
     client = await db.connect(req);
     if (area) {
       const areaArr = await shopDB.getShopByArea(client, area);
-      res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.SHOP_BY_AREA_SUCCESS, areaArr));
+      let responseData = duplicatedDataClean(areaArr, 'shopId', 'category');
+      const imagePromise = responseData.map((item) => {
+        const shopId = item.shopId;
+        return shopDB.getImageByShopId(client, shopId);
+      });
+
+      // TODO 이미지 데이터 들어오는 포맷 보고 데이터 붙이기
+      Promise.allSettled(imagePromise).then((image) => {
+        image.forEach((result, idx) => {
+          if (result.status === 'fulfilled') {
+            console.log('성공함');
+          } else if (result.status === 'rejected') {
+            console.log('[IMAGE PROMISE REJECTED]');
+          }
+        });
+      });
+
+      responseData.map((item) => {
+        if (!item.image) {
+          item.image = null;
+        }
+      });
+
+      res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.SHOP_BY_AREA_SUCCESS, responseData));
     }
     if (theme) {
       const themeArr = await shopDB.getShopByTheme(client, theme);
-      res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.SHOP_BY_THEME_SUCCESS, themeArr));
+      const responseData = duplicatedDataClean(themeArr, 'shopId', 'category');
+      const imagePromise = responseData.map((item) => {
+        const shopId = item.shopId;
+        return shopDB.getImageByShopId(client, shopId);
+      });
+
+      // TODO 이미지 데이터 들어오는 포맷 보고 데이터 붙이기
+      Promise.allSettled(imagePromise).then((image) => {
+        image.forEach((result, idx) => {
+          if (result.status === 'fulfilled') {
+            console.log('성공함');
+          } else if (result.status === 'rejected') {
+            // console.log('[IMAGE PROMISE REJECTED]');
+          }
+        });
+      });
+
+      responseData.map((item) => {
+        if (!item.image) {
+          item.image = null;
+        }
+      });
+      res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.SHOP_BY_THEME_SUCCESS, responseData));
     }
   } catch (error) {
     console.log(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
