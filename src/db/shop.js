74c9bd5d6c0c557e3891550cab3d8af979becae4
silver <silver36c@gmail.com>
2022-01-16 16:09:33 +0900

    ✏️ [CORRECT] : 소품샵 대표이미지 ORM 오타 수정 #20
    SELECT 여러개 하는데 ,가 없어서 ,추가

--- src/db/shop.js
+++ src/db/shop.js
@@ -64,7 +64,7 @@ const getShopByTheme = async (client, theme, sort, offset, limit) => {
 const getPreviewImageByShopId = async (client, shopId) => {
   const { rows } = await client.query(
     `
-        SELECT si.image si.shop_id as shopId
+        SELECT si.image, si.shop_id as shopId
         FROM shop_image si
         WHERE si.shop_id = $1
             AND is_preview = true
