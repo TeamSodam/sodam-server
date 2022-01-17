ac7b46edec822de5d9974d41dada4381b3444342
nahee-park <jessica396@ewhain.net>
2022-01-18 01:47:15 +0900

    [FIX] : 카테고리 스트링 배열로 오도록 수정 #41
    # ✨ [FEAT] : 새로운 기능 구현 #이슈번호
    # 🔨 [FIX] : 버그, 오류 해결 #이슈번호
    # 📝 [DOCS] : README나 WIKI 등의 문서 개정 #이슈번호
    # 💄 [STYLE] :  코드 포맷팅, 세미콜론 누락, 코드 변경이 없는 경우 #이슈번호
    # ♻️ [REFACTOR] : 코드 리펙토링 #이슈번호
    # ⚡️ [TEST] : 테스트 코드, 리펙토링 테스트 코드 추가 #이슈번호
    # ✅ [CHORE] : 빌드 업무 수정, 패키지 매니저 수정 #이슈번호
    # 🧭 [DEV] :  개발 환경 세팅 #이슈번호
    # ➕ [ADD] : Feat 이외의 부수적인 코드 추가, 라이브러리 추가, 새로운 파일 생성 시 #이슈번호
    # 🚑️ [HOTFIX] : issue나, QA에서 급한 버그 수정에 사용 #이슈번호
    # ⚰️ [DEL] : 쓸모없는 코드 삭제 #이슈번호
    # ✏️ [CORRECT] : 주로 문법의 오류나 타입의 변경, 이름 변경 등에 사용합니다. #이슈번호
    # 🚚 [MOVE] : 프로젝트 내 파일이나 코드의 이동 #이슈번호
    # ⏪️ [RENAME] : 파일 이름 변경이 있을 때 사용합니다. #이슈번호
    # 🔀 [MERGE]: 다른브렌치를 merge 할 때 사용합니다. #이슈번호
    # —————————
    #     제목은 명령문 대문자 영어로 적자.
    #     제목 끝에 마침표(.) 금지
    #     제목과 본문을 한 줄 띄워 분리하기
    #     본문은 한글로 적는다.
    #     본문은 "어떻게" 보다 "무엇을", "왜"를 설명한다.
    #     본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    # —————————

--- src/routes/shop/shopCategoryGET.js
+++ src/routes/shop/shopCategoryGET.js
@@ -4,60 +4,58 @@ const { shopDB } = require('../../db');
 const db = require('../../db/db');
 const util = require('../../lib/util');
 const { duplicatedDataClean } = require('../../lib/convertRawDataToProccessedData');
-const router = require('../user');
 
-const typeArray = ['문구팬시','인테리어소품','주방용품','패션소품','공예품','인형장난감'];
+const typeArray = ['문구팬시', '인테리어소품', '주방용품', '패션소품', '공예품', '인형장난감'];
 function randomItem(a) {
-    return a[Math.floor(Math.random() * a.length)];
+  return a[Math.floor(Math.random() * a.length)];
 }
 
-module.exports = async(req,res) =>{
-    const {type} = req.query;
-    if(!type){
-        return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+module.exports = async (req, res) => {
+  const { type } = req.query;
+  if (!type) {
+    return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+  }
+  let client;
+
+  try {
+    client = await db.connect(req);
+    let shopArr;
+    if (type == 'random') {
+      const randomType = randomItem(typeArray);
+      shopArr = await shopDB.getShopByCategory(client, randomType);
+    } else {
+      shopArr = await shopDB.getShopByCategory(client, type);
     }
-    let client; 
 
+    const categoryList = duplicatedDataClean(shopArr, 'shopId', 'category');
+    const imagePromise = categoryList.map((item) => {
+      const shopId = item.shopId;
+      return shopDB.getPreviewImageByShopId(client, shopId);
+    });
 
-    try{
-        client = await db.connect(req);
-        let categoryList;
-        if(type == 'random'){
-            const randomType = randomItem(typeArray)
-            categoryList = await shopDB.getShopByCategory(client,  randomType);
+    // TODO 이미지 데이터 들어오는 포맷 보고 데이터 붙이기
+    Promise.allSettled(imagePromise).then((image) => {
+      image.forEach((result) => {
+        if (result.status === 'fulfilled') {
+          console.log('성공함');
+        } else if (result.status === 'rejected') {
+          //     console.log('리젝티드됨');
         }
-        else{
-            categoryList = await shopDB.getShopByCategory(client, type);
-        }
+      });
+    });
 
-        const imagePromise = categoryList.map((item) => {
-            const shopId = item.shopId;
-            return shopDB.getPreviewImageByShopId(client, shopId);
-        });
-    
-          // TODO 이미지 데이터 들어오는 포맷 보고 데이터 붙이기
-        Promise.allSettled(imagePromise).then((image) => {
-            image.forEach((result) => {
-                if (result.status === 'fulfilled') {
-                console.log('성공함');
-                } else if (result.status === 'rejected') {
-                //     console.log('리젝티드됨');
-                }
-            });
-        });
+    categoryList.map((item) => {
+      if (!item.image) {
+        item.image = null;
+      }
+    });
 
-        categoryList.map((item)=>{
-            if(!item.image){
-                item.image = null;
-            }
-        });
-        
-        console.log(categoryList);
-        res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.SHOP_BY_CATEGORY_SUCCESS, categoryList));
-    } catch(error) {
-        console.log(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
-        res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
-    } finally{
-        client.release();
-    }
-}
+    console.log(categoryList);
+    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.SHOP_BY_CATEGORY_SUCCESS, categoryList));
+  } catch (error) {
+    console.log(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
+    res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+  } finally {
+    client.release();
+  }
+};
