bbfa2995369050bc1b766d61f1598de9218f8bad
silver <silver36c@gmail.com>
2022-01-18 08:07:07 +0900

    ✨ [FEAT] : 리뷰 등록 라우터 구현 #40

--- src/routes/review/index.js
+++ src/routes/review/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { checkUser } = require('../../middlewares/auth');
+const { uploadImage } = require('../../middlewares/uploadImage');
 
 // TODO: auth API 완성되면 checkUser 넣으면 됨
+router.post('/', uploadImage, require('./reviewPOST'));
 router.post('/:reviewId/like', require('./reviewLikePOST'));
 router.post('/:reviewId/scrap', require('./reviewScrapPOST'));
 router.get('/:shopId', require('./reviewShopIdSortPageGET'));
 
 module.exports = router;
-
