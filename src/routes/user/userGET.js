80d3c8f1c5315c16d488098f5e2630347cb4a537
nahee-park <jessica396@ewhain.net>
2022-01-21 01:49:57 +0900

    test: 배포 콘솔 테스트2

--- src/routes/user/userGET.js
+++ src/routes/user/userGET.js
@@ -18,7 +18,7 @@ module.exports = async (req, res) => {
     client = await db.connect(req);
     const user = await userDB.getUserById(client, userId);
     const test = {
-      test: 'pm2 restart',
+      test: '스크립트야 제발 돌아라!',
       user: user,
     };
 
