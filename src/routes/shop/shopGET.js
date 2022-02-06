aeb0809f8317196aa8706acdd731dbde5d0d3de2
nahee-park <jessica396@ewhain.net>
2022-02-06 21:21:18 +0900

    ♻️ [REFACTOR] : 지역별, 테마별 소품샵 가져오기 코드 리팩토링  #90

--- src/routes/shop/shopGET.js
+++ src/routes/shop/shopGET.js
@@ -6,11 +6,16 @@ const util = require('../../lib/util');
 const { duplicatedDataClean } = require('../../lib/convertRawDataToProccessedData');
 const slackAPI = require('../../middlewares/slackAPI');
 
+// 지역별 소품샵 - 쿼리로 area, sort를 받음
+// 테마별 소품샵 - 쿼리로 theme, sort, offset, limit을 받음
 module.exports = async (req, res) => {
   //sort쿼리 popular, mysave (지역별에서) , popular, review(테마별에서)
   const { area, theme, offset, limit, sort } = req.query;
   let pageOffset;
   let pageLimit;
+
+  pageOffset = Number((offset - 1) * limit);
+  pageLimit = limit;
 
   if (theme && !offset) {
     pageOffset = 0;
@@ -18,8 +23,6 @@ module.exports = async (req, res) => {
   if (theme && !limit) {
     pageLimit = 20;
   }
-  pageOffset = Number((offset - 1) * limit);
-  pageLimit = limit;
   if (!area && !theme) {
     return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
   }
@@ -29,15 +32,18 @@ module.exports = async (req, res) => {
   try {
     let responseData;
     client = await db.connect(req);
+    // 지역별 소품샵 불러오기 로직
     if (area) {
       const areaArr = await shopDB.getShopByArea(client, area, sort);
       responseData = duplicatedDataClean(areaArr, 'shopId', 'category');
+
+      // 이미지 데이터는 promise배열로 받고 풀어서 response데이터에 붙임
       const imagePromise = responseData.map((item) => {
         const shopId = item.shopId;
         return shopDB.getPreviewImageByShopId(client, shopId);
       });
       const previewImageObj = {};
-      // TODO 이미지 데이터 들어오는 포맷 보고 데이터 붙이기
+
       await Promise.allSettled(imagePromise).then((image) => {
         image.map((result) => {
           if (result.status === 'fulfilled') {
@@ -57,18 +63,23 @@ module.exports = async (req, res) => {
         }
       });
 
+      // 테마별 소품샵 불러오기 로직
       if (sort === 'mysave') {
         // 로그인 했으면 db에서 데이터 가져오기
         if (req.user) {
           // 북마크된 shopId를 가져와서 이미 가져온 responseData에서 해당 shopId에 해당하는 소품샵만 반환
           const bookmarkedShopId = await shopDB.getBookmarkedShopIdByUserIdAndArea(client, area, req.user[0].id);
+
+          // 저장된 소품샵이 없는 경우
           if (bookmarkedShopId.length === 0) {
             responseData = [];
             return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.SAVED_SHOP_EMPTY, responseData));
           }
+
+          // 저장된 소품샵이 있는 경우
           const bookmarkedShopIdArr = bookmarkedShopId.map((obj) => obj.shopId);
           if (bookmarkedShopIdArr.length === 0) {
-            const responseData = [];
+            responseData = [];
             return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.SAVED_SHOP_EMPTY, responseData));
           } else {
             responseData = responseData.filter((o) => {
@@ -80,18 +91,19 @@ module.exports = async (req, res) => {
           return res.status(statusCode.UNAUTHORIZED).send(util.fail(statusCode.UNAUTHORIZED, responseMessage.NEED_LOGIN));
         }
       }
-
       res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.SHOP_BY_AREA_SUCCESS, responseData));
     }
     if (theme) {
+      // 해당 테마에 해당하는 소품샵 데이터를 불러옴
       responseData = await shopDB.getShopByTheme(client, theme, sort, pageOffset, pageLimit);
-      // responseData = duplicatedDataClean(themeArr, 'shopId', 'category');
-      // console.log('responseData',responseData);
+
+      // 이미지 데이터 promise 배열로 받음
       const imagePromise = responseData.map((item) => {
         const shopId = item.shopId;
         return shopDB.getPreviewImageByShopId(client, shopId);
       });
 
+      // 카테고리 데이터 promise 배열로 받음
       const categoryPromise = responseData.map((item) => {
         const shopId = item.shopId;
         return shopDB.getCategoryAndIdByShopId(client, shopId);
@@ -99,7 +111,8 @@ module.exports = async (req, res) => {
 
       const previewImageObj = {};
       const categoryObj = {};
-      // TODO 이미지 데이터 들어오는 포맷 보고 데이터 붙이기
+
+      // promise 배열을 순서대로 품
       await Promise.allSettled(imagePromise).then((image) => {
         image.map((result) => {
           if (result.status === 'fulfilled') {
@@ -121,6 +134,8 @@ module.exports = async (req, res) => {
           }
         });
       });
+
+      // 풀어낸 promise 배열 데이터를 reponseData에 붙임
       responseData.map((item) => {
         if (previewImageObj[item.shopId]) {
           item.image = [previewImageObj[item.shopId].image];
