9e1819347e1558ae72f938c7e8f93c75c300f3a3
yoorimchoi <20201015@sungshin.ac.kr>
2022-02-10 15:03:40 +0900

    🔨 [FIX] : 요구된 데이터 길이보다 짧은 데이터 반환되는 오류 해결 #96

--- src/db/shop.js
+++ src/db/shop.js
@@ -213,19 +213,17 @@ const getShopBookmarkByUserId = async (client, shopId, userId) => {
 const getShopByBookmarkCounts = async (client, count) => {
   const { rows } = await client.query(
     `
-    SELECT s2.id as shop_id, s2.shop_name, c.name as category
-    FROM (
-        SELECT * FROM shop s
-        ORDER BY s.bookmark_count DESC
-        LIMIT $1
-      ) AS s2
-    INNER JOIN shop_category sc
-    ON s2.id = sc.shop_id
-    INNER JOIN category c
-    ON sc.category_id = c.id
-    WHERE
-      s2.is_deleted = FALSE
-      AND sc.is_deleted = FALSE
+      SELECT s.id as shop_id, s.shop_name, c.name as category
+      FROM shop s 
+      INNER JOIN shop_category sc
+      ON s.id = sc.shop_id
+      INNER JOIN category c
+      ON sc.category_id = c.id
+      WHERE 
+        s.is_deleted = FALSE
+        AND sc.is_deleted = FALSE
+      ORDER BY s.bookmark_count DESC
+      LIMIT $1
     `,
     [count],
   );
