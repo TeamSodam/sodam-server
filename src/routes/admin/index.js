f4a915286b85b289ca1ac845fb1a42d90c24a755
nahee-park <jessica396@ewhain.net>
2022-02-20 17:26:57 +0900

    ✨ [FEAT] : 전체 리뷰 가져오기 api 개발 #121

--- src/routes/admin/index.js
+++ src/routes/admin/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/shop/all', require('./allShopGET'));
+router.get('/review/all', require('./allReviewGET'));
 
 module.exports = router;
