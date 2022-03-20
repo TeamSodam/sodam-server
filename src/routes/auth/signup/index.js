54490a9a34f70acdbc2790ab51972a4650e3290f
silver <silver36c@gmail.com>
2022-03-21 04:19:42 +0900

    ✨ [FEAT] : 닉네임 중복 체크 #127

--- src/routes/auth/signup/index.js
+++ src/routes/auth/signup/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', require('./signupPOST'));
+router.post('/nickname', require('./signupNicknamePOST'));
 
 module.exports = router;
