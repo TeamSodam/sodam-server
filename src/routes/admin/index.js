0598a1c14665e90781953d2b8fecddaa9c02e91a
nahee-park <jessica396@ewhain.net>
2022-06-17 05:07:44 +0900

    test

--- src/routes/admin/index.js
+++ src/routes/admin/index.js
@@ -5,6 +5,6 @@ const { checkAdminUser } = require('../../middlewares/adminAuth');
 router.get('/shop/all', checkAdminUser, require('./allShopGET'));
 router.get('/shop/data', checkAdminUser, require('./shopDataGET'));
 router.get('/review/all', checkAdminUser, require('./allReviewGET'));
-router.post('/login', require('./adminLoginPOST'));
+// router.post('/login', require('./adminLoginPOST'));
 
 module.exports = router;
