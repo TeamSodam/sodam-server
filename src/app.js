899df414e3786b1db57acffcee77ee14fc190d03
yoorimChoi <yulim3839@naver.com>
2022-06-23 23:54:04 +0900

    🚑️ [HOTFIX] : cors origin 여러 개 추가

--- src/app.js
+++ src/app.js
@@ -7,13 +7,7 @@ dotenv.config();
 
 const allowedOrigins = ['https://sodam.me', 'https://sodam-client.vercel.app', 'https://server.sodam.me']
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
+  origin: allowedOrigins,
   credentials: true,
 };
 
