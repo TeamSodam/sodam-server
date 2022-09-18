7329b052997a401d2916c919705299807f2c79b2
silver <silver36c@gmail.com>
2022-09-19 05:16:53 +0900

    FEAT: 스프레드시트 크롤링이미지_최종 파일 삽입 코드 추가

--- src/dbscript/insertShopImage.js
+++ src/dbscript/insertShopImage.js
@@ -1,9 +1,17 @@
-// 이미지 없는 소품샵 리스트_이미지 url
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 const keys = require('../../keystoGoogleApis.json');
 const db = require('../db/db');
 const { sheetDB } = require('../db');
-const doc = new GoogleSpreadsheet('1v4Y4g5HJ2PMmD12yPKdW0FwmmqMrYWganIV9cyHQCnY');
+
+// ==============================================
+// IMPORTANT: 필요한 시트 주석 해제하고 사용하세요
+// ==============================================
+
+// 크롤링이미지_최종
+// const doc = new GoogleSpreadsheet('1T9UUGs010dilS1phcmVq-s1gF1jOo-obV7bqvPBqSLU');
+
+// 이미지 없는 소품샵 리스트_이미지 url
+// const doc = new GoogleSpreadsheet('1v4Y4g5HJ2PMmD12yPKdW0FwmmqMrYWganIV9cyHQCnY');
 
 let client;
 
@@ -21,14 +29,16 @@ const readSheet = async () => {
     client = await db.connect();
     await authGoogleSheet();
     const sheet = await doc.sheetsByIndex[0];
-    const rows = await sheet.getRows({ offset: 0, limit: 100 });
+    // offset 1: 컬럼명 제외
+    const rows = await sheet.getRows({ offset: 1, limit: 1500 });
     const result = rows.map((item) => {
       // console.log('shop_image', item._rawData[0], item._rawData[1], item._rawData[2], item._rawData[3]);
       const shop_id = item._rawData[0];
       const image_no = item._rawData[1];
       const image_url = item._rawData[2];
       const is_preview = item._rawData[3];
-      if (image_url.includes('search')) {
+      // 해당 url은 이미지가 안 열림 (삽입할 이미지 수정 필요)
+      if (!image_url.includes('postfiles')) {
         return sheetDB.insertShopImageData(client, shop_id, image_url, is_preview);
       } else {
         console.log('wrong image', item._rawData[0], item._rawData[1]);
